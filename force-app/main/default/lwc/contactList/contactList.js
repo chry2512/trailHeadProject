12df05ed082b06169dcd008def3cb4d743e93023
Chris <christianniro18@gmail.com>
2023-03-30 00:10:31 +0200

    initial commit

--- force-app/main/default/lwc/contactList/contactList.js
+++ force-app/main/default/lwc/contactList/contactList.js
@@ -1,8 +1,8 @@
 /**
  * @description       : 
- * @author            : 
+ * @author            : Christian Niro
  * @group             : 
- * @last modified on  : 03-28-2023
+ * @last modified on  : 03-30-2023
  * @last modified by  : 
 **/
 import { LightningElement, wire } from 'lwc';
@@ -10,6 +10,7 @@ import FIRSTNAME_FIELD from '@salesforce/schema/Contact.FirstName';
 import LASTNAME_FIELD from '@salesforce/schema/Contact.LastName';
 import  EMAIL_FIELD from '@salesforce/schema/Contact.Email';
 import getContacts from '@salesforce/apex/ContactController.getContacts';
+import { reduceErrors } from 'c/ldsUtils';
 const COLUMNS = [
     { label: 'Contact FirstName', fieldName: FIRSTNAME_FIELD.fieldApiName, type: 'text' },
     { label: 'Contact LastName', fieldName: LASTNAME_FIELD.fieldApiName, type: 'text' },
@@ -19,4 +20,9 @@ export default class ContactList extends LightningElement {
     columns = COLUMNS;
     @wire(getContacts)
     contacts;
+
+    get errors() {
+        return (this.contacts.error) ?
+            reduceErrors(this.contacts.error) : [];
+    }
 }
